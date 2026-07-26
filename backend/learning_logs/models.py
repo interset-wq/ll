d3b3509f7654098eaf31dcd8b77e3b8d59010e90
interset-wq <intersetwq@qq.com>
2026-07-26 20:50:35 +0800

    feat(backend): create admin and models

--- backend/learning_logs/models.py
+++ backend/learning_logs/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_added', 'text']
+
+    def __str__(self):
+        return self.text
+
+
+class Entry(models.Model):
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_added', 'text']
+
+    def __str__(self):
+        return self.text
