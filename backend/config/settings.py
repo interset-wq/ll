5d36749bab7dd20e4ed76ed89b3b3da6309d7525
interset-wq <intersetwq@qq.com>
2026-07-26 20:34:18 +0800

    feat(backend): create learning_logs app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My APP
+    'learning_logs',
+    
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Packages
 ]
 
 MIDDLEWARE = [
