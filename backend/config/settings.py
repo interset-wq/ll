869fcc41132eb4aa279957d1a3bae64c1705bf0f
interset-wq <intersetwq@qq.com>
2026-07-26 22:56:40 +0800

    feat(backend): user auth and ownership

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -123,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Redirect
+LOGIN_URL = 'accounts:login'
--- backend/learning_logs/views.py
+++ backend/learning_logs/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -10,16 +12,20 @@ def index(request):
     return render(request, template_name='learning_logs/index.html')
 
 
+@login_required
 def topics(request):
-    ts = Topic.objects.order_by('date_added')
+    ts = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {
         'topics': ts,
     }
     return render(request, template_name='learning_logs/topics.html', context=context)
 
 
+@login_required
 def topic(request, pk):
     t = get_object_or_404(Topic, pk=pk)
+    if t.owner != request.user:
+        raise Http404
     context = {
         'topic': t,
         'entries': t.entry_set.order_by('-date_added'),
@@ -27,11 +33,14 @@ def topic(request, pk):
     return render(request, template_name='learning_logs/topic.html', context=context)
 
 
+@login_required
 def new_topic(request):
     if request.method == 'POST':
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            t = form.save(commit=False)
+            t.owner = request.user
+            t.save()
             return redirect('learning_logs:topics')
     else:
         form = TopicForm()
@@ -40,6 +49,7 @@ def new_topic(request):
     return render(request, template_name='learning_logs/new_topic.html', context=context)
 
 
+@login_required
 def new_entry(request, pk):
     t = get_object_or_404(Topic, pk=pk)
     if request.method == 'POST':
@@ -56,9 +66,12 @@ def new_entry(request, pk):
     return render(request, template_name='learning_logs/new_entry.html', context=context)
 
 
+@login_required
 def edit_entry(request, pk):
     e = get_object_or_404(Entry, pk=pk)
     t = e.topic
+    if t.owner != request.user:
+        raise Http404
     if request.method == 'POST':
         form = EntryForm(data=request.POST, instance=e)
         if form.is_valid():
